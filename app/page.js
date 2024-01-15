3aa6c7f4fe36b1e0f87549c0f3d47d428a5718d6
zk-devex <156691903+zk-devex@users.noreply.github.com>
2024-01-15 16:48:07 +0000

    homepage w-full-screen

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 export default function Home(props) {
   return (
     <div>
-      <div className="max-h-screen bg-slate-400">
+      <div className="h-screen bg-slate-400">
         <div>
         <h1 className="text-5xl text-black font-bold">Home Page</h1>
         </div>
